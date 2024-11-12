7fe9168543b94fb039aa818534949f7b47673891
Xertypo22 <ajloansergey98@gmail.com>
2024-11-12 13:30:08 +0300

    version-2+sumOfNumbers

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 while True:
-    n1, n2, n3, n4, n5 = input("Введите пять чисел через пробел:\n").split()
-    print("Выберите как вы хотите вывести эти числа:")
-    print("1. В том же порядке")
-    print("2. В обратном порядке")
+  numbers = input("Введите пять чисел через пробел:\n").split()
 
-    while True:
-        choice = input()
-        try:
-            choice = int(choice)  # Преобразуем ввод в целое число
-            if 1 <= choice <= 2:
-                if choice == 1:
-                    print(n1, n2, n3, n4, n5)
-                else:
-                    print(n5, n4, n3, n2, n1)
-                break
-            else:
-                print("Некорректный вариант")
-        except ValueError:
-            print("Введите число от 1 до 2")
+  if len(numbers) != 5:
+    print("Введено некорректное количество чисел. Пожалуйста, введите пять чисел.")
+    continue
+
+  print("Выберите как вы хотите вывести эти числа:")
+  print("1. В том же порядке")
+  print("2. В обратном порядке")
+  print("3. Сумма чисел") # Добавлено: пункт меню для суммы
+
+  while True:
+    choice = input()
+    try:
+      choice = int(choice)
+      if choice == 1:
+        print(*numbers) # Вывод чисел в том же порядке
+        break
+      elif choice == 2:
+        print(*numbers[::-1]) # Вывод чисел в обратном порядке
+        break
+      elif choice == 3:
+        total = sum(float(num) for num in numbers)
+        print(f"Сумма чисел: {total}") # Добавлено: вычисление и вывод суммы
+        break
+      else:
+        print("Некорректный вариант")
+    except ValueError:
+      print("Введите число от 1 до 3")
+
