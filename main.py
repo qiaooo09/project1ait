145e376e893f983a9961cd783bfca4c22a53a95e
Xertypo22 <ajloansergey98@gmail.com>
2024-11-12 13:29:35 +0300

    optimized version-1

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 while True:
-    n1, n2, n3, n4, n5 = input("Введите пять чисел через пробел:\n").split()
+    numbers = input("Введите пять чисел через пробел:\n").split()
+
+    if len(numbers) != 5:
+        print("Введено некорректное количество чисел. Пожалуйста, введите пять чисел.")
+        continue
+
     print("Выберите как вы хотите вывести эти числа:")
     print("1. В том же порядке")
     print("2. В обратном порядке")
@@ -7,14 +12,16 @@ while True:
     while True:
         choice = input()
         try:
-            choice = int(choice)  # Преобразуем ввод в целое число
-            if 1 <= choice <= 2:
-                if choice == 1:
-                    print(n1, n2, n3, n4, n5)
-                else:
-                    print(n5, n4, n3, n2, n1)
+            choice = int(choice)
+            if choice == 1:
+                print(*numbers)  # Вывод чисел в том же порядке
+                break
+            elif choice == 2:
+                print(*numbers[::-1])  # Вывод чисел в обратном порядке
                 break
             else:
                 print("Некорректный вариант")
         except ValueError:
             print("Введите число от 1 до 2")
+
+
